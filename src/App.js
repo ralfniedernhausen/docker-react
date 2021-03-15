6c4544660d9f9cb4dd04045af9de44b1dc7239cd
Wolfgang Mauder <w.maud@web.de>
2021-03-15 13:22:04 +0100

    App and App.test copied from downloads, test changed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on Feature Branch
+          Learn React automatic Pull Request
         </a>
       </header>
     </div>
