7db7f93ef08ea74309fd4b75aa154e9e9be6e58c
Wolfgang Mauder <w.maud@web.de>
2021-03-15 11:25:49 +0100

    Initi

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for Free
         </a>
       </header>
     </div>
